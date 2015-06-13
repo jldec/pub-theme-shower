9795f77a7d7b8c1196df62b33b1da7da7520d10e
Jurgen Leschner <jldec@ciaosoft.com>
2015-06-13 16:12:00 -0700

    mv nav.css pub-theme-shower-ribbon.css

--- pub-config.js
+++ pub-config.js
@@ -11,7 +11,7 @@ module.exports = {
   staticPaths: [
     { path:'./node_modules/shower-core/shower.min.js', route:'/shower', inject:true },
     { path:'./node_modules/shower-ribbon/styles/screen.css', route:'/shower/ribbon/styles', inject:true },
-    { path:'./css/nav.css', route:'/css', inject:true },
+    { path:'./css/pub-theme-shower-ribbon.css', route:'/css', inject:true },
     { path:'./node_modules/shower-ribbon/images', route:'/shower/ribbon/images' },
     { path:'./node_modules/shower-ribbon/fonts', route:'/shower/ribbon/fonts' },
     { path:'./node_modules/shower-core/License.md', route:'/shower' },
